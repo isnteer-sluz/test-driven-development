cf53e8f564fae80df639bef36c4bda5c00e522e1
isnteer <146852753+isnteer@users.noreply.github.com>
2025-04-14 19:47:45 +0200

    Implement formatDuration to convert seconds into hours, minutes, and seconds

--- src/duration.ts
+++ src/duration.ts
@@ -1,4 +1,17 @@
 export function formatDuration(seconds: number): string {
-  console.log(seconds);
-  throw new Error("not implemented yet");
+
+  if (seconds < 0) throw new Error("seconds must be non-negative.");
+
+  const hour = Math.floor(seconds / 3600);
+  seconds = seconds % 3600;
+  const minute = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  const second = seconds;
+  
+  let retVal = "";
+  if (hour > 0) retVal += `${hour}h`;
+  if (minute > 0) retVal += `${minute}m`;
+  if (second > 0) retVal += `${second}s`;
+  
+  return retVal;
 }
