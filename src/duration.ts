988f334690a76ccfb0dfb366566ba90ac72a380b
isnteer <146852753+isnteer@users.noreply.github.com>
2025-04-14 20:00:37 +0200

    round seconds, return '0s' for zero

--- src/duration.ts
+++ src/duration.ts
@@ -2,6 +2,8 @@ export function formatDuration(seconds: number): string {
 
   if (seconds < 0) throw new Error("seconds must be non-negative.");
 
+  seconds = Math.round(seconds);
+
   const hour = Math.floor(seconds / 3600);
   seconds = seconds % 3600;
   const minute = Math.floor(seconds / 60);
@@ -12,6 +14,8 @@ export function formatDuration(seconds: number): string {
   if (hour > 0) retVal += `${hour}h`;
   if (minute > 0) retVal += `${minute}m`;
   if (second > 0) retVal += `${second}s`;
+
+  if (retVal === "") retVal = "0s";
   
   return retVal;
 }
